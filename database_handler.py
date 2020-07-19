6896130b6bf18e1a9272dc702a024eb351df8b03
Gaurav Arora <gauravarora.daiict@gmail.com>
2020-07-19 08:40:08 +0200

    Changes for MariaDB

--- database_handler.py
+++ database_handler.py
@@ -1,51 +1,56 @@
-import postgresql
 import json
+import mariadb
+
 
 class DatabaseHandler:
     def __init__(self, creds):
         c = json.load(open(creds))
-        connection_string = """pq://{user}:{password}@{host}/{db}""".format(user=c['user'],
-                   password=c['password'],
-                   host=c['host'],
-                   db=c['db'])
-        print(connection_string)
-        self.db = postgresql.open(connection_string)
+        try:
+            conn = mariadb.connect(
+                user=c['user'],
+                password=c['password'],
+                host=c['host'],
+                port=3306,
+                database=c['db']
+            )
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
+
+        # Get Cursor
+        self.db = conn.cursor()
+
 
     def create_user(self, chat_id, timezone):
         deletesql = """delete from users where chat_id='{chat_id}'""".format(chat_id=chat_id)
-        dsql = self.db.prepare(deletesql)
-        dsql()
+        self.db.execute(deletesql)
 
         sql = """insert into users (chat_id, timezone, created_on) values
                  ('{chat_id}', '{timezone}', current_timestamp);
                """.format(chat_id=chat_id, timezone=timezone)
-        insert_user = self.db.prepare(sql)
-        insert_user();
+        self.db.execute(sql)
 
     def create_reminder(self, chat_id, reminder_name, reminder_time):
         deletesql = """delete from reminders where chat_id='{chat_id}' and reminder_name='{reminder_name}'""".format(chat_id=chat_id, 
                        reminder_name=reminder_name)
-        dsql = self.db.prepare(deletesql)
-        dsql()
+        self.db.execute(deletesql)
 
         sql = """insert into reminders (chat_id, reminder_name, reminder_time, created_on) values
                  ('{chat_id}', '{reminder_name}', '{reminder_time}', current_timestamp);
                """.format(chat_id=chat_id, reminder_name=reminder_name, reminder_time=reminder_time)
-        insert_user = self.db.prepare(sql)
-        insert_user()
+        self.db.execute(sql)
 
     def delete_reminder(self, chat_id, reminder_name):
         sql = """delete from reminders where chat_id='{chat_id}' and reminder_name='{reminder_name}'
               """.format(chat_id=chat_id, reminder_name=reminder_name)
-        delete_user = self.db.prepare(sql)
-        delete_user()
+        self.db.execute(sql)
 
     def get_all_reminder(self):
         sql = """select users.chat_id as chat_id, timezone, reminder_name, reminder_time from reminders rem join users users on rem.chat_id = users.chat_id"""
-        all_reminders = self.db.prepare(sql)
-        return all_reminders()
+        self.db.execute(sql)
+        return list(self.db)
 
     def get_all_users(self):
         sql = """select * from users"""
-        all_reminders = self.db.prepare(sql)
-        return all_reminders()
+        self.db.execute(sql)
+        return list(self.db)
